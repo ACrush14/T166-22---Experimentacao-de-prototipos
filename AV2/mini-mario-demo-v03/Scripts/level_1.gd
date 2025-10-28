dacaa47cc26ffb0f0ac17476f94d5e5fafa88d44
Anderson (Crush) de Lima <96736515+ACrush14@users.noreply.github.com>
2025-10-28 10:13:37 -0300

    Delete AV2 directory

--- AV2/mini-mario-demo-v03/Scripts/level_1.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _on_historia_2_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Cenas/historia_2.tscn")
-
-func _on_fundo_morte_body_entered(body: Player) -> void:
-	if(body.is_dead == false):
-		body._die()
